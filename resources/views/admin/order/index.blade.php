3934f28ae3076c6887926e50077b2b2760461cd0
sanjay dahal <sanjay.dahal90@gmail.com>
2026-09-17 21:21:13 +0545

    Add timestamp to page title to force unique page load

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -1,10 +1,11 @@
 @extends('admin.includes.main')
-@section('title', 'Orders')
+@section('title', 'Orders - {{ time() }}')
 
 @push('head')
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta http-equiv="Pragma" content="no-cache">
 <meta http-equiv="Expires" content="0">
+<meta name="cache-buster" content="{{ time() }}">
 @endpush
 
 @section('content')
