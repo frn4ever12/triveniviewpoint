d182a099ffd0a5e03c083dc02c50e2214e5f6929
sanjay dahal <sanjay.dahal90@gmail.com>
2026-09-17 21:18:39 +0545

    Add cache-buster meta tag to force browser refresh

--- resources/views/admin/includes/main.blade.php
+++ resources/views/admin/includes/main.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
+    <meta name="cache-buster" content="{{ time() }}">
     @include('admin.includes.top')
     @yield('title')
     @stack('styles')
