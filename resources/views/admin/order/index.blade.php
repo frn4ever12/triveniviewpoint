a62c4c095ae61792f84e42733d7e6db35ba95090
sanjay dahal <sanjay.dahal90@gmail.com>
2026-09-17 21:28:33 +0545

    Remove URL timestamp redirect to fix caching issues

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -5,17 +5,6 @@
 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
 <meta http-equiv="Pragma" content="no-cache">
 <meta http-equiv="Expires" content="0">
-<meta name="cache-buster" content="{{ time() }}">
-<script>
-    // Force reload with timestamp to bypass all caches
-    (function() {
-        var url = new URL(window.location.href);
-        if (!url.searchParams.has('_t')) {
-            url.searchParams.set('_t', Date.now());
-            window.location.replace(url.toString());
-        }
-    })();
-</script>
 @endpush
 
 @section('content')
